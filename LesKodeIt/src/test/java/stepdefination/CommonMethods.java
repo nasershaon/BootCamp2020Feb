823295b419295df655f04c92682d021e6c69f5ec
nasershaon <bbshaon@gmail.com>
2020-02-26 21:43:16 -0500

    update

---
+++ LesKodeIt/src/test/java/stepdefination/CommonMethods.java
@@ -0,0 +1,24 @@
+package stepdefination;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import testhomepage.TestHomepage;
+
+import java.io.IOException;
+
+public class CommonMethods extends TestHomepage {
+
+    @Given("I am at Letskodeit Homepage")
+    public void i_am_at_Letskodeit_Homepage() throws IOException {
+        // Write code here that turns the phrase above into concrete actions
+    setUp(false,"","windows","10","chrome","79","https://learn.letskodeit.com/");
+    }
+
+
+    @Then("I Close Browser")
+    public void i_Close_Browser() {
+        // Write code here that turns the phrase above into concrete actions
+    driver.quit();
+    }
+
+}
